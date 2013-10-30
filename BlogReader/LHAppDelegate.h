5b09c43a8bd02bee253e827e9a27bb082f762f51
Lukas Herbst <lukas@lukass-mbp.fritz.box>
2013-10-30 21:28:07 +0100

    Initial Commit

---
+++ BlogReader/LHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LHAppDelegate.h
+//  BlogReader
+//
+//  Created by Lukas Herbst on 30.10.13.
+//  Copyright (c) 2013 Lukas Herbst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
